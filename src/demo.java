1f644d27e170a27db0cb393bbfcb2ec058c60076
Justayak <jutanke@googlemail.com>
2012-10-24 14:43:38 +0200

    init commit

---
+++ src/demo.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Baka
+ * Date: 24.10.12
+ * Time: 14:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class demo {
+    public static void main(String[]args){
+        System.out.println("hallo1");
+    }
+}
