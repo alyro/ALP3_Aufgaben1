c1ef9360f239cdb5a87c63e8892bb8744d134280
Justayak <jutanke@googlemail.com>
2012-10-27 21:46:11 +0200

    Trapez + Viereck

--- src/ab1/aufgabe3/Trapez.java
+++ src/ab1/aufgabe3/Trapez.java
@@ -1,5 +1,8 @@
 package ab1.aufgabe3;
 
+import java.awt.*;
+import java.awt.geom.Line2D;
+
 /**
  * Created with IntelliJ IDEA.
  * ALP III - Julian
@@ -10,4 +13,42 @@ package ab1.aufgabe3;
  * To change this template use File | Settings | File Templates.
  */
 public class Trapez extends Viereck {
+
+    protected double h;
+
+    public Trapez(Point a, Point b, Point c, Point d){
+        super(a,b,c,d);
+        // check if is Trapez
+        Line2D ab = new Line2D.Float(a,b);
+        Line2D cd = new Line2D.Float(c,d);
+        if (this.calculateSlope(ab) == this.calculateSlope(cd)){
+            this.h = ab.ptLineDist(d);
+        }else{
+            Line2D ad = new Line2D.Float(a,d);
+            Line2D bc = new Line2D.Float(b,c);
+            if (this.calculateSlope(ad) == this.calculateSlope(bc)){
+                this.h = ad.ptLineDist(c);
+            }else{
+                throw new IllegalArgumentException("At least 2 lines must be parallel to form a Trapez");
+            }
+        }
+    }
+
+    @Override
+    public double flaeche(){
+        return ((this.getAB() + this.getCD())/2) * this.h;
+    }
+
+    /**
+     * calulates the slope of a line
+     * @param line
+     * @return
+     */
+    private double calculateSlope(Line2D line){
+        double x1 = line.getP1().getX();
+        double y1 = line.getP1().getY();
+        double x2 = line.getP2().getX();
+        double y2 = line.getP2().getY();
+        return (y2 - y1)/(x2 - x1);
+    }
 }
--- src/ab1/aufgabe3/Viereck.java
+++ src/ab1/aufgabe3/Viereck.java
@@ -1,5 +1,7 @@
 package ab1.aufgabe3;
 
+import java.awt.*;
+
 /**
  * Created with IntelliJ IDEA.
  * ALP III - Julian
@@ -10,18 +12,100 @@ package ab1.aufgabe3;
  * To change this template use File | Settings | File Templates.
  */
 public class Viereck implements Figur {
+
+    protected final Point a;
+    protected final Point b;
+    protected final Point c;
+    protected final Point d;
+    protected double flaeche = -1;
+    protected double umfang = -1;
+
+    /**
+     *
+     * @param a Bottom-Left
+     * @param b Bottom-Right
+     * @param c Top-Right
+     * @param d Top-Left
+     */
+    public Viereck(Point a, Point b, Point c, Point d){
+        // check position of buttons
+        // ATTENTION: Natural orientation is used ->
+        // (0/0) is Bottom-Left and NOT Top-Left
+        if (a.getY() >= d.getY() || a.getX() >= b.getX() ||// check Point a
+            b.getY() >= c.getY() || // check Point b
+            c.getX() <= d.getX() // check Point c
+                // all checks for Point d are done
+                ) {
+            throw new IllegalArgumentException("The orientation of the Points is wrong. Check documentation");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+
+    // Getter
+
+    public Point getA(){
+        return this.a;
+    }
+
+    public Point getB(){
+        return this.b;
+    }
+
+    public Point getC(){
+        return this.c;
+    }
+
+    public Point getD(){
+        return this.d;
+    }
+
+    protected double getAB(){
+        return this.a.distance(this.b);
+    }
+
+    protected double getBC(){
+        return this.b.distance(this.c);
+    }
+
+    protected double getCD(){
+        return this.c.distance(this.d);
+    }
+
+    protected double getAD(){
+        return this.a.distance(this.d);
+    }
+
+
     @Override
     public double flaeche() {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+         if (this.flaeche < 0){
+             // we only need to calculate this once (lazy loading)
+             // gaußsche Trapezformel:
+             this.flaeche =
+                     (1/2) * Math.abs(
+                        ((this.a.getY() - this.c.getY())*
+                        (this.d.getX() - this.b.getX()))+
+                        ((this.b.getY() - this.d.getY())*
+                        (this.a.getX() - this.c.getX()))
+                     );
+         }
+         return this.flaeche;
     }
 
     @Override
     public double umfang() {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        if (this.umfang < 0){
+            // we only need to calculate this once (lazy loading)
+            this.umfang = this.getAB() + this.getBC()+ this.getCD() + this.getAD();
+        }
+        return this.umfang;
     }
 
     @Override
     public int compareTo(Figur o) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        return Double.compare(this.flaeche(), o.flaeche());
     }
 }
