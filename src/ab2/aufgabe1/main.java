ad2324ec5d2b445bff01f92c042fa86a8a38d018
Justayak <jutanke@googlemail.com>
2012-11-04 15:48:14 +0100

    ab2 aufgabe1

---
+++ src/ab2/aufgabe1/main.java
@@ -0,0 +1,16 @@
+package ab2.aufgabe1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * ALP III - Julian
+ * <p/>
+ * User: Baka
+ * Date: 04.11.12
+ * Time: 15:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class main {
+    public static void main(String[]args){
+
+    }
+}
